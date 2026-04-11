e2497eee0552500197ec13b8d471acf4dd7c56b3
ibrahim <iharby2002@gmail.com>
2026-04-11 17:10:36 +0800

    feat: integrate React Query and Mantine notifications

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,21 @@ import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
 import { RouterProvider } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Notifications } from "@mantine/notifications";
 import "./index.css";
+import "@mantine/notifications/styles.css";
 import "@mantine/core/styles.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
-      <RouterProvider router={AppRoutes} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={AppRoutes} />
+        <Notifications />
+      </QueryClientProvider>
     </MantineProvider>
   </StrictMode>,
 );
