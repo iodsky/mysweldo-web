4ca9bb38a3c4b0fce6e2e559ba92d73ebd41fea0
ibrahim <iharby2002@gmail.com>
2026-04-17 12:12:56 +0800

    chore: Ignore eslint.config.js and enforce TS unused-vars
    - add /eslint.config.js to .prettierignore to prevent Prettier from formatting the ESLint config.
    - update eslint.config.js to import rules from eslint-plugin-react-hooks
    - update esling.config.js to add a rules override that disables the base "no-unused-vars" and enables "@typescript-eslint/no-unused-vars" as an error.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
+import reactHooks, { rules } from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
@@ -20,4 +20,9 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-]);
+],   {
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },);
